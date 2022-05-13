983820a2e6e307f4411662bca1944a2d1c796502
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-13 17:09:26 +0200

    Added Form to login-view and registration-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Form, Button, Navbar, Nav, Row, Container } from 'react-bootstrap';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -16,22 +17,45 @@ export function LoginView(props) {
 }
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label><br></br>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label><br></br>
-      <button type="submit" onClick={handleSubmit}>Already User-LogIn Here</button><br></br>
-      <button
+    <Container fluid>
+      <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">My-Flix</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="./login-view.jsx">LogIn</Nav.Link>
+        <Nav.Link href="#profile">Profile</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+   </Navbar>
+    <Form>
+    <Row className="justify-content-md-center">
+    <Form.Group controlId="formUsername">
+      <Form.Label>Username:</Form.Label>
+      <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+    </Form.Group>
+    </Row><br></br>
+    <Row className="justify-content-md-center">
+    <Form.Group controlId="formPassword">
+      <Form.Label>Password:</Form.Label>
+      <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+    </Form.Group>
+    </Row><br></br>
+    <Row className="justify-content-md-center">
+     <Button variant="primary" type="submit" onClick={handleSubmit}>
+      Already User-LogIn
+    </Button>
+    </Row><br></br>
+    <Row className="justify-content-md-center">
+    <Button variant="primary"
         type="submit"
         onClick={handleRegistration}
       >
        New User-Register Here
-      </button>
-    </form>
+      </Button>
+      </Row>
+  </Form>
+  </Container>
   );
 }
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Form, Button, Navbar, Nav, Row, Container } from 'react-bootstrap';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -13,24 +14,49 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label><br></br>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label><br></br>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label><br></br>
-      <label>
-        Birthday:
-        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label><br></br>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container fluid>
+      <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">My-Flix</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="./login-view.jsx">LogIn</Nav.Link>
+        <Nav.Link href="#profile">Profile</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+   </Navbar>
+    <Form>
+    <Row className="justify-content-md-center">
+    <Form.Group controlId="formUsername">
+      <Form.Label>Username:</Form.Label>
+      <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+    </Form.Group>
+    </Row><br></br>
+    <Row className="justify-content-md-center">
+    <Form.Group controlId="formPassword">
+      <Form.Label>Password:</Form.Label>
+      <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+    </Form.Group>
+    </Row><br></br>
+    <Row className="justify-content-md-center">
+    <Form.Group controlId="formEmailID">
+      <Form.Label>EmailID:</Form.Label>
+      <Form.Control type="email" placeholder="Email" onChange={e => setPassword(e.target.value)} />
+    </Form.Group>
+    </Row><br></br>
+    <Row className="justify-content-md-center">
+    <Form.Group controlId="formBirthday">
+      <Form.Label>Birthday:</Form.Label>
+      <Form.Control type="date" placeholder="Birthdate" onChange={e => setPassword(e.target.value)} />
+    </Form.Group>
+    </Row><br></br>
+    <Row className="justify-content-md-center">
+     <Button variant="primary" type="submit" onClick={handleSubmit}>
+      Submit
+    </Button>
+    </Row>
+  </Form>
+  </Container>
   );
 }
