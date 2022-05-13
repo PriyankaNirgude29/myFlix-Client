9d9a29ae9039d9ee7c6dbba4b90a3824929d9e64
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-13 22:22:03 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Navbar, Nav, Row, Container } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -14,49 +14,35 @@ export function RegistrationView(props) {
   };
 
   return (
-    <Container fluid>
-      <Navbar bg="dark" expand="lg" variant="dark">
-          <Navbar.Brand href="#home">My-Flix</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="./login-view.jsx">LogIn</Nav.Link>
-        <Nav.Link href="#profile">Profile</Nav.Link>
-      </Nav>
-      </Navbar.Collapse>
-   </Navbar>
-    <Form>
-    <Row className="justify-content-md-center">
-    <Form.Group controlId="formUsername">
-      <Form.Label>Username:</Form.Label>
-      <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
-    </Form.Group>
-    </Row><br></br>
-    <Row className="justify-content-md-center">
-    <Form.Group controlId="formPassword">
-      <Form.Label>Password:</Form.Label>
-      <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-    </Form.Group>
-    </Row><br></br>
-    <Row className="justify-content-md-center">
-    <Form.Group controlId="formEmailID">
-      <Form.Label>EmailID:</Form.Label>
-      <Form.Control type="email" placeholder="Email" onChange={e => setPassword(e.target.value)} />
-    </Form.Group>
-    </Row><br></br>
-    <Row className="justify-content-md-center">
-    <Form.Group controlId="formBirthday">
-      <Form.Label>Birthday:</Form.Label>
-      <Form.Control type="date" placeholder="Birthdate" onChange={e => setPassword(e.target.value)} />
-    </Form.Group>
-    </Row><br></br>
-    <Row className="justify-content-md-center">
-     <Button variant="primary" type="submit" onClick={handleSubmit}>
-      Submit
-    </Button>
-    </Row>
-  </Form>
-  </Container>
+    <Row className="d-flex justify-content-center">
+       <Col xs={4} md={4} >
+       <Form className="d-flex flex-column justify-content-between align-items-center border border-black ">
+           <Form.Group controlId="formUsername">
+           <Form.Label>Username:</Form.Label>
+           <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+           </Form.Group>
+
+          <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+          </Form.Group>
+
+          <Form.Group controlId="formEmailID">
+          <Form.Label>EmailID:</Form.Label>
+          <Form.Control type="email" placeholder="Email" onChange={e => setPassword(e.target.value)} />
+          </Form.Group>
+
+          <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control type="date" placeholder="Birthdate" onChange={e => setPassword(e.target.value)} />
+          </Form.Group><br></br>
+
+         <Button variant="primary" type="submit" onClick={handleSubmit}>
+             Submit
+          </Button>
+       </Form>
+    </Col>
+  </Row>
+
   );
 }
