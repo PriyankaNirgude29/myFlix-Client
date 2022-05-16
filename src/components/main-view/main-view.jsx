dd9bdc34fd87a29784629d22def1a59515a9a7f6
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-16 15:21:19 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -18,25 +18,42 @@ constructor(){
 }
 
 componentDidMount(){
-    axios.get('https://my-movie-api29.herokuapp.com/movies')
-      .then(response => {
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
+  let accessToken = localStorage.getItem('token');
+  if(accessToken!==null){
+    this.setState({
+      user: localStorage.getItem('user')
+    });
+    this.getMovies(accessToken);
+    }
   }
 
 setSelectedMovie(newSelectedMovie) { 
      this.setState({selectedMovie: newSelectedMovie });
      }
 
-     onLoggedIn(user) {
-        this.setState({
-          user
-        });
+     onLoggedIn(authData) {
+      console.log(authData);
+      this.setState({
+        user: authData.user.Username
+      });
+    
+      localStorage.setItem('token', authData.token);
+      localStorage.setItem('user', authData.user.Username);
+      this.getMovies(authData.token);
+      }
+
+      getMovies(token){
+        axios.get('https://my-movie-api29.herokuapp.com/movies',{
+              headers: { Authorization: 'Bearer ${token}'}
+           })
+        .then(response => {
+                this.setState({
+                movies: response.data
+             });
+          })
+         .catch(error => {
+              console.log(error);
+          });
       }
 
       onRegister(registered) {
