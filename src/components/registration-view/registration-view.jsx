428c925b38507ebed38d0dd86f9679bd9403ecbd
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-18 21:14:14 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Form, Button, Row, Col } from 'react-bootstrap';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -10,14 +10,48 @@ export function RegistrationView(props) {
   const [ email, setEmail ] = useState('');
   const [ birthday, setBirthday ] = useState('');
 
+  //Declare hook for each input
+  const [usernameErr, setUsernameErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+  const [emailErr, setEmailErr] = useState('');
+
+
+  const validate = () => {
+    let isReq = true; 
+    if(!username){
+      setUsernameErr('Create Username');
+      isReq = false; 
+    } else if(username.length < 5){
+      setUsernameErr('Username must be 5 characters long');
+      isReq = false;
+    }
+    if(!password){
+      setPasswordErr('Create Password(Min 6 characters)');
+      isReq=false;
+    }else if (password.length < 6){
+      setPasswordErr('Password must be 6 characters long');
+      isReq=false;
+    }
+    if(!email){
+      setEmailErr('Add Email');
+      isReq = false;
+    } else if(email.indexOf('@') === -1){
+      setEmail('Invalid Email');
+      isReq = false; 
+    }
+   
+    return isReq;
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-      axios
-        .post("https://my-movie-api29.herokuapp.com/users", {
-          username: username,
-          password: password,
-          email: email,
-          birthday: birthday,
+    const isReq = validate();
+    if (isReq){
+      axios.post("https://my-movie-api29.herokuapp.com/users", {
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthday: birthday
         })
         .then((response) => {
           const data = response.data;
@@ -30,7 +64,7 @@ export function RegistrationView(props) {
           console.error(response);
           alert("error registering the user");
         });
-    
+      }
   };
 
   return (
@@ -60,17 +94,28 @@ export function RegistrationView(props) {
          <Button variant="primary" type="submit" onClick={handleSubmit}>
              Submit
           </Button>
+          <p> Already have an account?</p>
+                <Link to={`/`}>
+                  <Button
+                    variant="primary"
+                    className="custom-btn"
+                    type="submit"
+                  >
+                    Sign in
+                  </Button>
+                </Link>
        </Form>
     </Col>
   </Row>
 
   );
 }
+
 RegistrationView.propTypes = {
   register: PropTypes.shape({
     Username: PropTypes.string.isRequired,
     Password: PropTypes.string.isRequired,
     Email: PropTypes.string.isRequired,
     Birthday: PropTypes.string.isRequired,
-  }).isRequired,
+  }),
 };
