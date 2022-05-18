6416993aad57fe06c4b1119debf64ce6790c3eaf
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-18 22:27:52 +0200

    Update user-data.jsx

--- src/components/profile-view/user-data.jsx
+++ src/components/profile-view/user-data.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
 
 export function UserData(props) {
-  const { userdata } = props;
+  const userdata = props.userdata;
 
   return (
     <Fragment>
-      <h1 className="text-primary">{userdata.userName}</h1>
+      <h1 className="text-primary">{userdata.username}</h1>
       <p>Email: {userdata.email}</p>
       <p>Birthday: {userdata.birthday}</p>
     </Fragment>
