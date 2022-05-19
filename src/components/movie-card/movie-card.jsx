835220f62be48a05ccb5600f28f0bf06cdce2f90
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-19 15:45:00 +0200

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,47 @@
 import React from "react";
+import { useState, useEffect }  from "react";
 import PropTypes from "prop-types";
 import './movie-card.scss';
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import axios from 'axios';
 
-
+import { Link } from 'react-router-dom';
 export class MovieCard extends React.Component {
-  
-  render() {
-    const { movie, onMovieClick } = this.props;
+constructor() {
+  super();
+
+  this.state = {
+    FavoriteMovies: []
+  };
+}
+
+onAddFavorite = (movie) => {
+  const Username = localStorage.getItem('user');
+  const token = localStorage.getItem('token');
+
+  axios.post(
+    `https://my-movie-api29.herokuapp.com/users/${Username}/movies/${movie._id}`,
+    {
+      FavoriteMovies: this.state.FavoriteMovies
+    },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => {
+      this.setState({
+        FavoriteMovies: response.data.FavoriteMovies
+      });
+      console.log(response);
+      alert("Movie Added to favorities!!");
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+ render() {
+    const { movie, onAddFavorite } = this.props;
 
     return( 
      
@@ -24,6 +55,14 @@ export class MovieCard extends React.Component {
         <Card.Text className='movie-description'>{movie.Description}</Card.Text>
         <Link to={`/movies/${movie._id}`}>
         <Button className='open-button' variant="link">Open</Button>
+        {!this.state.favourite && (
+            <Button
+              variant="outline-secondary"
+              onClick={() => this.onAddFavorite(movie)}
+            >
+              Add to favourite
+            </Button>
+          )}
       </Link>
       </Card.Body>
     </Card>
