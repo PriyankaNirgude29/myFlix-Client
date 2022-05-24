584125b24ac00a6510e392f6f5b9cee6939859d0
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-24 22:03:04 +0200

    Create visibility-filter.jsx

---
+++ src/components/visibility-fliter/visibility-filter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Form from 'react-bootstrap/Form';
+
+import { setFilter } from '../../actions/actions';
+
+function VisibilityFilterInput(props) {
+    const { visibilityFilter, setFilter }=props;
+  return (
+  <Form.Control
+    className='border border-dark'
+    onChange={(e) => setFilter(e.target.value)}
+    value={visibilityFilter}
+    placeholder="Search Movies"
+  /> );
+}
+
+export default connect(null,{ setFilter })(VisibilityFilterInput);
