820eb20ce841101837bc8519ce284dc17a5bf794
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-05-25 18:43:07 +0200

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axios from "axios";
+import { Link } from "react-router-dom";
 
-import { ListGroup, Button, Row, Col, Image} from 'react-bootstrap';
-import './movie-view.scss';
+import { Button, Image} from 'react-bootstrap';
+//import './movie-view.scss';
 
 export class MovieView extends React.Component {
   
@@ -25,30 +25,44 @@ export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
     return (
-      <Row className="w-100 justify-content-around mx-auto">
-        <Col sm={5}> 
-        	<Image className="poster" src={movie.ImagePath} crossOrigin="anonymous" onClick={() => onMovieClick(movie)}/>
-        </Col>
+      <div className="movie-view my-3">
+      <div className="movie-poster">
+        <img
+          src={movie.ImagePath}
+          crossOrigin="anonymous"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="movie-title">
+        <span className="label">Title: </span>
+        <span className="value">{movie.Title}</span>
+      </div>
+      <div className="movie-description">
+        <span className="label">Description: </span>
+        <span className="value">{movie.Description}</span>
+      </div>
+      <div className="movie-genre">
+        <span className="label">Genre: </span>
+        <Link className="value" to={`/genre/${movie.Genre.Name}`}>
+          {movie.Genre.Name}
+        </Link>
+      </div>
+      <div className="movie-director">
+        <span className="label">Director: </span>
+        <Link className="value" to={`/director/${movie.Director.Name}`}>
+          {movie.Director.Name}
+        </Link>
+      </div>
 
-        <Col sm={6}>   
-			<ListGroup>
-				<ListGroup.Item><h3>{movie.Title}</h3></ListGroup.Item>
-        <ListGroup.Item>Description: {movie.Description}</ListGroup.Item>
-				<ListGroup.Item>Genre: {movie.Genre.Name}</ListGroup.Item>
-        <ListGroup.Item>Director: {movie.Director.Name}</ListGroup.Item>
-			
-				<div>
-					
-
-            <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
-            
-					
-				</div>
-				
-			</ListGroup> 
-        </Col>
-	
-    </Row>
+      <Button
+        onClick={() => {
+          onBackClick(null);
+        }}
+      >
+        Back
+      </Button>
+    </div>
     );
   }
 }
